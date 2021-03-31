38737f624d0731d7a613ad02211e4e3a6a9eebc1
chandu256 <81565889+chandu256@users.noreply.github.com>
2021-03-31 09:22:52 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,12 @@
+class area
+{
+public static void main(String a[])
+{
+int radius=10;
+final float PI=3.14f;
+float area=PI*radius*radius;
+float perimeter=2*3.14f*radius;
+System.out.println("Area of circle="+area);
+System.out.println("perimeter of circle="+perimeter);
+}
+}
